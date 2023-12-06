feef6bd9b5fc0a5d77395a7ef404ad423543caad
thorblock <135666868+thorblock@users.noreply.github.com>
2023-12-06 13:28:57 -0800

    markers created

--- static/js/logic.js
+++ static/js/logic.js
@@ -9,7 +9,20 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// chi said to use mapbox, but i don't think there has ever been a mapbox example in class
+// function to pick size of marker by magnitude
+function markerSize(magnitude) {
+    return magnitude * 5;
+};
+// function to pick color of marker by depth
+function markerColor(depth) {
+    if (depth <= 10) return "rgb(236, 235, 249)";
+    else if (depth <= 30) return "rgb(214, 212, 241)";
+    else if (depth <= 50) return "rgb(191, 187, 244)";
+    else if (depth <= 70) return "rgb(157, 150, 244)";
+    else if (depth <= 90) return "rgb(121, 113, 232)";
+    else return "rgb(87, 77, 232)"
+};
+
 
 // hold off on layers for now, let's work geojson
 var earthquake_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
@@ -21,23 +34,42 @@ d3.json(earthquake_url).then(function(data) {
     console.log(data);
     // features groups, 
     var features = data.features;
-    var depth_array = [];
     // loop through returned data
     for (var i = 0; i <features.length; i++) {
+        // coordinates has 3 parts: lng, lat, and depth
         var coordinates = features[i].geometry.coordinates;
+        console.log(coordinates);
+        // lng
         var longitude =  coordinates[0];
+        // lat
         var latitude = coordinates[1];
+        // depth
+        depth = coordinates[2];
         
-        // id depth and push to premade depth_array
-        var depth = coordinates[2];
-        depth_array.push(depth);
-
+        // properties has a number of parts, but we only need 3 specifically: place, mag, and time
         var properties = features[i].properties;
-
-        // id place & magnitude
+        // place
         var place = properties.place;
+        // mag
         var magnitude = properties.mag;
+        // time ** having issues with the time setup, not sure how to make it legible
+        var time = dayjs(properties.time);
+
+        // actual earthquake markers
+        earthquakes = L.circleMarker([latitude,longitude], {
+            color: "black",
+            weight: 1,
+            fillColor: markerColor(depth),
+            opacity: 1,
+            fillOpacity: 1,
+            radius: markerSize(magnitude)
+        }).bindPopup(`<h3>${place}</h3><br/>Magnitude: ${magnitude}<br/>Depth: ${depth} km<br>Time: ${time}`).addTo(map)
 
 
-    }
+    };
+
+
+
+
+
 });
